5c504809795c5e3d14845f0e4e9f05690c73e978
mayukh551 <may14ukh@gmail.com>
2024-04-10 12:41:13 +0530

    changed time standard to IST format

--- middleware/log-request.js
+++ middleware/log-request.js
@@ -1,10 +1,9 @@
 function logRequest(req, res, next) {
     const date = new Date();
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
-    console.log(`Time ${hours}:${minutes}:${seconds}   Req: ${req.method}/ => ${req.url}`);
-    next()
+    const options = { timeZone: 'Asia/Kolkata', hour12: false };
+    const time = date.toLocaleString('en-IN', options);
+    console.log(`Time ${time}   Req: ${req.method}/ => ${req.url}`);
+    next();
 }
 
 module.exports = logRequest;
